ff4f329967c61c58cbaf13491b871ac7c42a2306
mrlolethan <ethanjoyce@rocketmail.com>
2015-02-24 18:48:55 -0330

    Initial commit.

---
+++ src/main/java/com/mrlolethan/myplugin/MyPlugin.java
@@ -0,0 +1,19 @@
+package com.mrlolethan.myplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+	}
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+}
